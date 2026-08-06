5be966e89ba8f1ebf94e8988d804c5ba611a72d4
Hien Do <hien.do@employmenthero.com>
2026-08-06 09:34:17 +0700

    perf: add 200ms debounce to Telescope grep

--- init.lua
+++ init.lua
@@ -547,11 +547,12 @@ do
     -- You can put your default mappings / updates / etc. in here
     --  All the info you're looking for is in `:help telescope.setup()`
     --
-    -- defaults = {
-    --   mappings = {
-    --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-    --   },
-    -- },
+    defaults = {
+      debounce = 200,
+      -- mappings = {
+      --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+      -- },
+    },
     -- pickers = {}
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
